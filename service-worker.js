cef6dd6ba45cbca127bd46ed793c1189fe89be2b
Denys Chemeriakov <chemeriakov@gmail.com>
2021-08-31 17:08:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.6c09234e6d5481ae450f2b8c89f52d31.js"
 );
 
 self.addEventListener('message', (event) => {
